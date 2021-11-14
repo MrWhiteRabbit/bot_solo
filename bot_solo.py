b9411cf73ddfcae30c002026c2427877ca9a1ee0
Max Godkin <mgodkin@gmail.com>
2021-11-14 21:36:01 +0300

    main funct

---
+++ bot_solo.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+import telebot
+import datetime
+from auth_data import auth_data
+
+
+bot = telebot.TeleBot(auth_data)
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == '/help':
+        bot.send_message(message.chat.id, '/age - покажу возраст в днях.')
+    elif message.text == '/age':
+        startdate = '2021-08-04'.split('-')
+        enddate = str(datetime.date.today()).split('-')  # '2021-11-14'.split('-')
+        sd = datetime.date(int(startdate[0]), int(startdate[1]), int(startdate[2]))
+        ed = datetime.date(int(enddate[0]), int(enddate[1]), int(enddate[2]))
+        fd = str(ed - sd).split()[0]
+        bot.send_message(message.chat.id, f'Сегодня мне {fd} дней!')
+
+
+bot.polling(none_stop=True, interval=0)
